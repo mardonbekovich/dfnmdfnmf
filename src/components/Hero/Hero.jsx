6a47483761698afe3ebe08eb2fb7b5ca0c0e0ff8
Mardonbek <163328721+mardonbekovich@users.noreply.github.com>
2024-11-19 19:12:24 +0500

    Add files via upload

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from './Hero.module.scss'
+const Hero = () => {
+  return (
+    <>
+      <section className={s.hero}>
+     
+        </section>
+    </>
+  );
+};
+
+export default Hero;
